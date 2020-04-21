ef7364679ec4648196938903739b0bceca80f9e8
AlexandreGhannad <63161526+AlexandreGhannad@users.noreply.github.com>
2020-04-21 10:02:11 +0200

    Create Copy_of_setup.m

---
+++ Copy_of_setup.m
@@ -0,0 +1,21 @@
+%% Clean
+clear all
+close all
+clc
+%% Setup 1
+pdcoo_home = pwd;
+
+addpath(pdcoo_home)
+addpath(fullfile(pdcoo_home, 'Formulations'))
+addpath(fullfile(pdcoo_home, 'Solvers'))
+addpath(fullfile(pdcoo_home, 'readmps'))
+addpath(fullfile(pdcoo_home, 'Tools'))
+addpath(fullfile(pdcoo_home, 'Problems\MPS'))
+addpath(fullfile(pdcoo_home, 'Validation-eigenvalue'))
+p = genpath(fullfile(pdcoo_home, 'addons'));
+addpath(p)
+if exist('Variants', 'dir') ~= 7
+  mkdir('Variants');
+end
+addpath(fullfile(pdcoo_home, 'Variants'))
+addpath(fullfile(pdcoo_home, 'Test'))
