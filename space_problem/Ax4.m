a197bb73cef8189ec84385e6a33bd75828a773fb
AlexandreGhannad <63161526+AlexandreGhannad@users.noreply.github.com>
2020-07-28 10:39:55 +0200

    extesion of the quarter problem

--- space_problem/Ax4.m
+++ space_problem/Ax4.m
@@ -1,6 +1,7 @@
 function Y = Ax4(x, n, m, rho1, epsilon, transpose)
 if transpose == 1
-    F(1:n, 1:n) = reshape(x(1:n^2), [n n]);
+    n2 = 2*n;
+    F = reshape(x(1:n2^2), [n2 n2]);
     
     Xs = ((0.5-n):(n-0.5))'/(2*n);
     Etas = (-m:m)'*rho1/m;
@@ -12,15 +13,15 @@ if transpose == 1
     Y = [Y2;Y3];
     
 elseif transpose == 2
-    m2 = m+1;
-    n2 = n-1;
+    m2 = 2*m+1;
+    n2 = 2*n;
     
     F1 = reshape(x(1:m2^2), [m2 m2]);
     F2 = reshape(x(m2^2+1:end), [m2 m2]);
     
-    Xs = ( (0.5-m2):(m2-0.5))'/(2*m2);
-    Etas = (-n2:n2)'*rho1/n2;
-    K = cos(2*pi*Etas*Xs')/(2*m2);
+    Xs = ( -m:m)'/(2*m);
+    Etas = (0.5-n:n-0.5)'*rho1/n;
+    K = cos(2*pi*Etas*Xs')/(2*m);
     Fhat1 = K*F1*K';
     Fhat2 = K*F2*K';
     Fhat1 = Fhat1(:) - epsilon * Fhat1(1);
